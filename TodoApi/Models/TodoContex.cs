f2aafb32405d99b9510b131dac8cf3c5e6acaf73
Дмитрий Давыдов <mega.davydov@inbox.ru>
2020-10-05 08:22:44 +0300

    Добавьте файлы проекта.

---
+++ TodoApi/Models/TodoContex.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class TodoContex : DbContext
+    {
+        public TodoContex(DbContextOptions<TodoContex> options ) : base(options)
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        
+    }
+}
